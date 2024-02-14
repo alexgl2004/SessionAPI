beda809bac727537c8dbedfdaa1d746317265cc0
Ralf Siewert <ralf@actyvy.st>
2024-02-14 16:24:07 +0100

    refactoring Actors.jsx

---
+++ src/components/FilterBox/index.jsx
@@ -0,0 +1,52 @@
+import styles from "./FilterBox.module.css";
+
+// boxStyle: "linklist" or "select"
+// data: [{key:"gryffindor", value:"Gryffindor"}]
+// onFilterChange: (key)=>{}
+
+export function FilterBox({ boxStyle, data, onFilterChange }) {
+  if (boxStyle === "linklist") {
+    return (
+      <div className={styles["filter-box"]}>
+        <button
+          onClick={() => {
+            onFilterChange(null);
+          }}
+        >
+          ALL
+        </button>
+        {data.map(({ key, value }) => {
+          return (
+            <button
+              key={key}
+              onClick={() => {
+                onFilterChange(key);
+              }}
+            >
+              {value}
+            </button>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <select
+        name="select_names"
+        className="search_help_buttons"
+        onChange={(event) =>
+          onFilterChange(
+            event.target.value === "ALL" ? null : event.target.value
+          )
+        }
+      >
+        <option value={"ALL"}>ALL</option>
+        {data.map(({ key, value }) => (
+          <option key={key} value={key}>
+            {value}
+          </option>
+        ))}
+      </select>
+    );
+  }
+}
--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -1,172 +1,114 @@
 import "./Actors.css";
-import { Link } from 'react-router-dom';
-import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import noimg from "../assets/noimg.png";
+import { FilterBox } from "../components/FilterBox";
 
 export default function Actors() {
+  const [characters, setCharacters] = useState([]);
+  const [selectedHouse, setSelectedHouse] = useState("");
+  const [selectedSpecies, setSelectedSpecies] = useState("");
 
-  const [characters, setCharacters] = useState([])
-  //const [houses, setHouses] = useState([])
-  const [house, setHouse] = useState("")
-  const [species, setSpecies] = useState("")
-
-  function setStateF(val,property){
-    switch(property){
-      case 'house':
-        setHouse(val)
-      break;
-      case 'species':
-        setSpecies(val)
-      break;
-    }
-
-  }
-
-  function outLabel(property){
-    switch(property){
-      case 'house':
-        return (
-          <><label className="label_search" htmlFor="charachter_search">Filter by House: </label>
-          <input name="charachter_search" type="text" value={house} onChange={(event) =>{
-            setHouse(event.target.value)
-          }}/></>
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const response = await axios.get(
+          "https://hp-api.onrender.com/api/characters"
         );
-      break;
-      case 'species':
-        return (
-          <><label className="label_search" htmlFor="charachter_search">Filter by Species: </label>
-          <input name="charachter_search" type="hidden" value={species} onChange={(event) =>{
-            setSpecies(event.target.value)
-          }}/></>
-        )
-      break;
-      default:
-      break;
-    }
-  
-  }
-
-  function makeSearchByProperty(property){
-
-    let temp_Houses = []
-
-    characters.forEach((character, index) => {
-      if(temp_Houses.indexOf(character[property])==-1 && character[property]){
-        temp_Houses.push(character[property])
+        console.log(response.data);
+        setCharacters(
+          response.data.filter((character) => (character.actor ? true : false))
+        );
+      } catch (error) {
+        console.log(error);
+        alert("Problem mit load data!");
       }
-    });    
-    switch(property){
-     case 'house':
-        return (
-          <>
-            <div className="search_help_buttons">
-             <button value="" onClick={(event) => setStateF(event.target.value,property)}>ALL</button>
-              {temp_Houses.map((house_m, index) => (
-                <button value={house_m} onClick={(event) => setStateF(event.target.value,property)}>{house_m}</button>
-              ))
-              }
-            </div>
-            {outLabel(property)}
-          </>
-        )
-      break;
-      case 'species':
-        return (
-          <>
-            {outLabel(property)}
-            <select name="select_names" className="search_help_buttons" onChange={(event) => setStateF(event.target.value,property)}>
-              <option value="">ALL</option>
-              {temp_Houses.map((house_m, index) => (
-                <option value={house_m}>{house_m}</option>
-              ))
-              }
-            </select>
-            
-          </>
-        )
-      break;
     }
-    
+    loadData();
+  }, []);
+
+  // derived state
+
+  function createFilterList(propName) {
+    return characters.reduce((acc, cur) => {
+      if (!cur[propName]) return acc;
+      if (acc.find((value) => cur[propName] === value)) {
+        return acc;
+      } else {
+        return [...acc, cur[propName]];
+      }
+    }, []);
   }
 
-  async function handleLoadData(){
-
-    try{
-
-      const response = await axios.get(
-        "https://hp-api.onrender.com/api/characters"
-      )
-      console.log(response.data)
-      setCharacters(response.data)
-
-    }catch (error){
-      console.log(error)
-      alert('Problem mit load data!')
-    }
-  }
-
-  if(characters[0]==undefined){
-    handleLoadData()
-  }
-
-
-  //console.log(temp_Houses)
-
-  let characters_out = characters;
-
-  if(house!=''){
-   // console.log(house)
-    characters_out = characters_out.filter((character) => {
-      //console.log(character.house.toUpperCase().indexOf(house.toUpperCase()), character.house, house)
-      return character.house.toUpperCase().indexOf(house.toUpperCase())!=-1
-    });
-  }else{
-   
-  }
-
-  if(species!=''){
-    // console.log(house)
-     characters_out = characters_out.filter((character) => {
-       //console.log(character.house.toUpperCase().indexOf(house.toUpperCase()), character.house, house)
-       return character.species.toUpperCase().indexOf(species.toUpperCase())!=-1
-     });
-   }else{
-     
-   }
-
- //  console.log(species,house)
+  const charactersToDisplay = characters.filter((character) => {
+    const houseMatch =
+      !selectedHouse ||
+      character.house.toUpperCase().indexOf(selectedHouse.toUpperCase()) !== -1;
+    const speciesMatch =
+      !selectedSpecies || character.species === selectedSpecies;
+    return speciesMatch && houseMatch;
+  });
 
   return (
     <>
-        <h2>Characters</h2>
-        <div>
-            <div className="Searches">
-            {makeSearchByProperty('house')}
-            {makeSearchByProperty('species')}
-            </div>
-            <ul className="Actors">
-              {characters_out.map((character, index) => (
-                <>
-                {!character.actor?'':(
-                <li key={character.id}>
-                  <Link className="actor_a" to={"/characters/" + character.id}>
-                    <div className="actors_block">
-                      <div className="actor_img">
-                          <img src={character.image?character.image:noimg} alt={character.name} />
-                      </div>
-                      <div className="actors_about">
-                        <h3>{character.name}<div><small>({character.actor})</small></div></h3>               
-                      </div>            
-                    </div>
-                  </Link>
-                </li>)}
-                </>
-                ))
-              }
-            </ul>
-            
+      <h2>Characters</h2>
+      <div>
+        <div className="Searches">
+          <input
+            type="text"
+            value={selectedHouse}
+            onChange={(event) => {
+              setSelectedHouse(event.target.value);
+            }}
+          />
+          <FilterBox
+            boxStyle="linklist"
+            data={createFilterList("house").map((house) => {
+              return { key: house, value: house };
+            })}
+            onFilterChange={(key) => {
+              setSelectedHouse(key);
+            }}
+          />
+
+          <FilterBox
+            boxStyle="select"
+            data={createFilterList("species").map((item) => {
+              return { key: item, value: item };
+            })}
+            onFilterChange={(key) => {
+              setSelectedSpecies(key);
+            }}
+          />
         </div>
+        <ul className="Actors">
+          {charactersToDisplay.map((character) => (
+            <>
+              <li key={character.id}>
+                <Link className="actor_a" to={"/characters/" + character.id}>
+                  <div className="actors_block">
+                    <div className="actor_img">
+                      <img
+                        src={character.image ? character.image : noimg}
+                        alt={character.name}
+                      />
+                    </div>
+                    <div className="actors_about">
+                      <h3>
+                        {character.name}
+                        <div>
+                          <small>({character.actor})</small>
+                        </div>
+                      </h3>
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            </>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
